b065078929bd71effe9b65d2c01a425f9b3cb219
Guilherme Isalino <guilhermeisalino12@gmail.com>
2023-10-20 21:29:17 -0300

    Imports e Criação de Classe/Pasta

---
+++ pizzaria/pizzaria/src/main/java/com/pizzaria/main/controller/ClienteController.java
@@ -0,0 +1,20 @@
+package com.pizzaria.main.controller;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
+/*import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView; */
+import org.springframework.stereotype.Controller;
+
+//import com.pizzaria.main.model;
+
+@Controller
+public class ClienteController {
+   // @Autowired
+    //private ClientRepository ClientRepository;
+
+}
---
+++ pizzaria/pizzaria/src/main/java/com/pizzaria/main/model/bd/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.pizzaria.main.model.bd;
+
+//import org.springframework.data.repository.CrudRepository;
+//import java.util.List;
+
+public class ClienteRepository {
+    
+}
