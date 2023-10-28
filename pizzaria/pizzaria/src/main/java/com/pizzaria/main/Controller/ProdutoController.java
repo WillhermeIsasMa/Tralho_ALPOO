7e7967f0737d7468e62a3bfde24d4b3f807dc38a
Guilherme Isalino <guilhermeisalino12@gmail.com>
2023-10-28 11:20:51 -0300

    Tentativa de puxar requisição pro site

--- pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ProdutoController.java
+++ pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ProdutoController.java
@@ -1,13 +1,14 @@
 package com.pizzaria.main.Controller;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.pizzaria.main.Model.Produto;
+//import com.pizzaria.main.Model.Produto;
 import com.pizzaria.main.Repositories.ProdutoRepository;
 
 @RestController
@@ -16,9 +17,16 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
-    @GetMapping
-    public List<Produto> findall(){
-        List<Produto> result = produtoRepository.findAll(); 
-        return result;
+    @GetMapping ("/cardapio")
+    public ModelAndView index() {
+        ModelAndView cardapio = new ModelAndView();
+        cardapio.setViewName("index");
+        cardapio.addObject("cardapio", produtoRepository.findAll());
+        return cardapio;
     }
+    /*public List<Produto> findall(){
+        List<Produto> result = produtoRepository.findAll();
+        result.addObject("cardapio", "MSG");
+        return result;
+    }*/
 }
