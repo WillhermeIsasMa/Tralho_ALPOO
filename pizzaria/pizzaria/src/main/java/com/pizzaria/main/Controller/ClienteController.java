969827dc0819d755ea5461459a15427887fc6055
Rennanpaulino <rennanpsousa2003@gmail.com>
2023-10-29 12:06:27 -0300

    commit controllers update Co-authored-by: Guilherme Isalino <WillhermeIsasMa@users.noreply.github.com>

--- pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ClienteController.java
+++ pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ClienteController.java
@@ -1,11 +1,12 @@
 package com.pizzaria.main.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.pizzaria.main.Model.Cliente;
 import com.pizzaria.main.Repositories.ClienteRepository;
@@ -17,8 +18,18 @@ public class ClienteController {
     private ClienteRepository clienteRepository;
 
     @GetMapping
-    public List<Cliente> findall(){
-        List<Cliente> result = clienteRepository.findAll();
+    public ModelAndView cliente(){
+    ModelAndView usuarios = new ModelAndView();
+    usuarios.setViewName("cliente");
+    usuarios.addObject("cliente", clienteRepository.findAll());
+    return usuarios;
+    }
+
+
+
+    @PostMapping
+    public Cliente insert(@RequestBody Cliente cliente){
+        Cliente result = clienteRepository.save(cliente);
         return result;
     }
     
--- pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ProdutoController.java
+++ pizzaria/pizzaria/src/main/java/com/pizzaria/main/Controller/ProdutoController.java
@@ -1,28 +1,37 @@
 package com.pizzaria.main.Controller;
 
-//import java.util.List;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-//import com.pizzaria.main.Model.Produto;
+import com.pizzaria.main.Model.Produto;
 import com.pizzaria.main.Repositories.ProdutoRepository;
 
 @RestController
-@RequestMapping(value = "/produtos")
+@RequestMapping(value = "/cardapio")
 public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
-    @GetMapping ("/cardapio")
+    @GetMapping
     public ModelAndView index() {
         ModelAndView cardapio = new ModelAndView();
         cardapio.setViewName("index");
         cardapio.addObject("cardapio", produtoRepository.findAll());
         return cardapio;
+    }
+
+    @PostMapping
+    public Produto insert(@RequestBody Produto produto){
+        Produto result = produtoRepository.save(produto);
+        return result;
     }
     /*public List<Produto> findall(){
         List<Produto> result = produtoRepository.findAll();
